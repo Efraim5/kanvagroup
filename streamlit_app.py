cb4faa5c79dd1b75f5f5c426fe3ac480bca69f00
Efraim5 <118956517+Efraim5@users.noreply.github.com>
2025-03-13 23:35:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-st.set_page_config(layout='wide')
 
 st.title("🏠Optimalisasi Harga Properti Airbnb di beberapa kota di Eropa")
 st.write(
